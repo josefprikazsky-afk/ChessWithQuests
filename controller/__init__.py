4c31b4f9525837e59b61413f24a44ef597e93d69
josefprikazsky-afk <josef.prikazsky@gjkt.eu>
2026-09-25 11:08:08 +0200

    Update __init__.py

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,66 +1,55 @@
-#inicializace balíku kontroleru
-from abc import ABC, abstractmethod
-from typing import List, Tuple, Optional, Any
-
 # ==========================================
-# 1. MODEL (Data a pravidla)
+# 3. CONTROLLER (Řídící logika)
 # ==========================================
 
-class Uzivatel:
-    def __init__(self, jmeno: str, elo: int = 1000):
-        self.jmeno = jmeno
-        self.elo = elo
+class GameManager:
+    def __init__(self, u1: Uzivatel, u2: Uzivatel):
+        self.plocha = HernaPlocha()
+        self.view = KonzoleView()
+        self.hraci = [Hrac("Bílá", u1), Hrac("Černá", u2)]
+        self.aktualni_index = 0
+        self.bezi = True
 
-class Hrac:
-    def __init__(self, barva: str, uzivatel: Uzivatel):
-        self.barva = barva
-        self.uzivatel = uzivatel
-
-class Figurka(ABC):
-    def __init__(self, barva: str, symbol_bily: str, symbol_cerny: str):
-        self.barva = barva
-        self.symbol = symbol_bily if barva == "Bílá" else symbol_cerny
-
-    def __str__(self):
-        return self.symbol
-
-class Kral(Figurka):
-    def __init__(self, barva): super().__init__(barva, "♔", "♚")
-class Dama(Figurka):
-    def __init__(self, barva): super().__init__(barva, "♕", "♛")
-class Vez(Figurka):
-    def __init__(self, barva): super().__init__(barva, "♖", "♜")
-class Strelec(Figurka):
-    def __init__(self, barva): super().__init__(barva, "♗", "♝")
-class Kun(Figurka):
-    def __init__(self, barva): super().__init__(barva, "♘", "♞")
-class Pesec(Figurka):
-    def __init__(self, barva): super().__init__(barva, "♙", "♟")
-
-class HernaPlocha:
-    def __init__(self):
-        self.rozmery = (8, 8)
-        self.deska: List[List[Optional[Figurka]]] = [[None for _ in range(8)] for _ in range(8)]
-        self.inicializuj_hru()
-
-    def inicializuj_hru(self):
-        # Rozestavení černých figurek (horní část)
-        hlavni_rada = [Vez, Kun, Strelec, Dama, Kral, Strelec, Kun, Vez]
-        for i, trida in enumerate(hlavni_rada):
-            self.deska[0][i] = trida("Černá")
-            self.deska[1][i] = Pesec("Černá")
+    def start_game(self):
+        self.view.zobraz_zpravu("ŠACHOVÁ PARTIE ZAČÍNÁ")
+        while self.bezi:
+            self.view.vykresli_plochu(self.plocha)
+            aktualni_hrac = self.hraci[self.aktualni_index]
             
-        # Rozestavení bílých figurek (spodní část)
-        for i, trida in enumerate(hlavni_rada):
-            self.deska[6][i] = Pesec("Bílá")
-            self.deska[7][i] = trida("Bílá")
+            odkud, kam = self.view.ziskej_tah(aktualni_hrac.uzivatel.jmeno, aktualni_hrac.barva)
+            
+            if odkud is None:
+                self.view.zobraz_zpravu("Hra byla ukončena uživatelem.")
+                break
 
-    def vrat_obsah(self, r: int, c: int) -> Optional[Figurka]:
-        return self.deska[r][c]
+            figurka = self.plocha.vrat_obsah(*odkud)
+            if figurka is None:
+                self.view.zobraz_zpravu("Chyba: Na zadané výchozí pozici není žádná figurka.")
+                continue
+                
+            if figurka.barva != aktualni_hrac.barva:
+                self.view.zobraz_zpravu("Chyba: Můžete táhnout pouze svými figurkami!")
+                continue
 
-    def posun_figurku(self, odkud: Tuple[int, int], kam: Tuple[int, int]):
-        r1, c1 = odkud
-        r2, c2 = kam
-        figurka = self.deska[r1][c1]
-        self.deska[r2][c2] = figurka
-        self.deska[r1][c1] = None
+            # Vykonání tahu
+            cileva_fig = self.plocha.vrat_obsah(*kam)
+            if cileva_fig and cileva_fig.barva == aktualni_hrac.barva:
+                self.view.zobraz_zpravu("Chyba: Nemůžete vyhodit vlastní figurku!")
+                continue
+
+            self.plocha.posun_figurku(odkud, kam)
+            if cileva_fig:
+                self.view.zobraz_zpravu(f"Vyhozen {cileva_fig.barva} {cileva_fig.__class__.__name__}!")
+
+            # Přepnutí hráče
+            self.aktualni_index = 1 - self.aktualni_index
+
+# ==========================================
+# SPUŠTĚNÍ HRY
+# ==========================================
+if __name__ == "__main__":
+    hrac1 = Uzivatel("Jan", 1500)
+    hrac2 = Uzivatel("Karel", 1450)
+    
+    hra = GameManager(hrac1, hrac2)
+    hra.start_game()
